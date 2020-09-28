3c4bbb8d007995d558ab84decfb16ca7b75cf882
Taomercy <taomercy@qq.com>
2020-09-28 14:53:20 +0800

    docker deployment

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+project_path=/home/ec2-user/CMS
+
+pushd ${project_path}
+    docker-compose down
+    image_id=`docker images | grep "blockchain_web" | awk '{print $3}'`
+    docker rmi ${image_id}
+    git reset --hard HEAD^ && git clean -xdf && git pull
+    docker-compose build && docker-compose up -d
+popd
+
+
